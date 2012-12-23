8f2209dbb01a59b4b986ad5ff4c75780bc507f83
Dante Passera <passera.dante@gmail.com>
2012-12-23 18:46:40 -0500

    initial

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	
+	<meta charset="utf-8" />
+	<title>Smashing Boxes Scheduler</title>
+
+	<link rel="stylesheet" href="css/style.css" />
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
+	<script src="https://apis.google.com/js/client.js" type="text/javascript"></script>
+	<script src="js/main.js" type="text/javascript"></script>
+	<!--[if IE]> <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+</head>
+
+<body>
+
+	<button onclick="auth();">Authorize</button>
+
+</body>
---
+++ js/main.js
@@ -0,0 +1,20 @@
+var GOOGLE_API_KEY = "AIzaSyBm2aZhHwkDvM-1K1N2v-TvwUGS2UnZlq0";
+// for localhost
+var CLIENT_ID = "969439793066.apps.googleusercontent.com";
+
+$(document).ready(function() {
+	//gapi.client.setApiKey(GOOGLE_API_KEY);
+	//auth();
+});
+
+function auth() {
+	var config = {
+		'client_id': CLIENT_ID,
+		'scope': 'https://www.googleapis.com/auth/urlshortener'
+
+	};
+	gapi.auth.authorize(config, function() {
+		console.log('login complete');
+		console.log(gapi.auth.getToken());
+	});
+}
