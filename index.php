f69a9ec267a594aec659a7af4ccdcab3b3c0ce78
Dante Passera <passera.dante@gmail.com>
2012-12-23 19:43:32 -0500

    gets all calendar events. adds test calendar event.

--- index.php
+++ index.php
@@ -21,6 +21,17 @@
 
 <body>
 
-	<button onclick="auth();">Authorize</button>
+	<div id="auth_container">
+		<button onclick="auth();">Authorize</button>
+	</div>
+
+	<div id="get_events_container" class="hidden">
+		<button onclick="getCalendarEvents();">Get events</button>
+	</div>
+
+	<div id="add_event_container" class="hidden">
+		Event name:<input id="input_event_name" type="text">
+		<button onclick="addEvent();">Add event</button>
+	</div>
 
 </body>
--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var GOOGLE_API_KEY = "AIzaSyBm2aZhHwkDvM-1K1N2v-TvwUGS2UnZlq0";
 // for localhost
 var CLIENT_ID = "969439793066.apps.googleusercontent.com";
 
+var accessToken = null;
+
 $(document).ready(function() {
 	//gapi.client.setApiKey(GOOGLE_API_KEY);
 	//auth();
@@ -13,8 +15,57 @@ function auth() {
 		'scope': 'https://www.googleapis.com/auth/calendar'
 
 	};
+
+	gapi.client.setApiKey(GOOGLE_API_KEY);
+
 	gapi.auth.authorize(config, function() {
-		console.log('login complete');
 		console.log(gapi.auth.getToken());
+		accessToken = gapi.auth.getToken().access_token;
+		handleAuthSuccess();
+	});
+}
+
+function handleAuthSuccess() {
+	$('#auth_container').addClass('hidden');
+	$('#get_events_container').removeClass('hidden');
+	$('#add_event_container').removeClass('hidden');
+}
+
+function handleAuthFailure() {
+
+}
+
+function addEvent() {
+	var name = $('#input_event_name').val();
+	var resource = {
+		'summary': name,
+		'location': 'Somewhere',
+		'start': {
+			'dateTime': '2012-12-25T10:00:00.000-07:00'
+		},
+		'end': {
+			'dateTime': '2012-12-25T12:00:00.000-07:00'
+		}
+	};
+	var request = gapi.client.calendar.events.insert({
+		'calendarId': 'primary',
+		'resource': resource
+	});
+	request.execute(function(resp) {
+		console.log(resp);
+	});
+}
+
+function getCalendarEvents() {
+	gapi.client.load('calendar', 'v3', function() {
+		var request = gapi.client.calendar.events.list({
+			'calendarId': 'primary'
+		})
+
+		request.execute(function(resp) {
+			for(var i=0; i<resp.items.length; i++) {
+				console.log(resp.items[i].summary);
+			}
+		});
 	});
 }
